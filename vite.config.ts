c04579e362c0930465e65d3eb0fb0c3c2ced258a
iLocaDL <lorenzolocatelli2003@gmail.com>
2026-02-15 22:24:17 +0100

    comm3

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Sito-ius/',
+  build: { outDir: 'docs' },
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
-
